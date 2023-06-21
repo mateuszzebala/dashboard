58301489124253b5ed5c7d94aad9638100eea5a6
mateuszzebala <mateusz.zebala.pl@gmail.com>
2023-06-21 20:36:16 +0200

    change api url

--- django/main/settings.py
+++ django/main/settings.py
@@ -10,11 +10,11 @@ DEBUG = SERVER_CONFIG.DEBUG()
 ALLOWED_HOSTS = SERVER_CONFIG.GET_ALLOWED_HOSTS()
 
 CORS_ALLOWED_ORIGINS = [
-    'http://localhost'
+    'http://localhost:3000'
 ]
 
 CSRF_TRUSTED_ORIGINS = [
-    'http://localhost'
+    'http://localhost:3000'
 ]
 
 CORS_ALLOW_CREDENTIALS = True
